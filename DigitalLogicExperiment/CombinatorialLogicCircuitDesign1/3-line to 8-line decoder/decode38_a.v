b8a2eec49d83fe0861416e6bacd4c78261c7e68a
farmer3-c <158997415+farmer3-c@users.noreply.github.com>
2025-05-30 19:09:21 +0800

    Create decode38_a.v

---
+++ DigitalLogicExperiment/CombinatorialLogicCircuitDesign1/3-line to 8-line decoder/decode38_a.v
@@ -0,0 +1,14 @@
+module decode38_a(
+    input wire [2:0] a,
+    output reg [7:0] y 
+    ); 
+    
+    integer i;
+    
+    always @ (*)
+        for(i=0;i<=7;i=i+1)
+            if(a==i) 
+                y[i] = 1;
+            else    
+                y[i] = 0;  
+endmodule
